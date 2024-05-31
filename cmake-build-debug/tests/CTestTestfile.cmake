3b54b6fd2b632887c3a07f2d1c95018b55a676c0
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2024-05-31 07:16:19 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/PE-Parser/tests
-# Build directory: D:/PE-Parser/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/PE-Parser/cmake-build-debug/tests/Tester[1]_include.cmake")
